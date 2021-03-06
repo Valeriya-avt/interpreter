bc640533fce20b8f7ab214567c13f9f5cb5c84f5
Valeriya Abdrakhmanova <valeriya.avt@gmail.com>
2021-03-06 11:10:42 +0200

    poliz

--- poliz.cpp
+++ poliz.cpp
@@ -14,15 +14,16 @@ class Lexem {
 public:
 	LEX_TYPE type;
 	Lexem() { };
-	int getType() {return type;}
-	virtual int getValue() { };
+	int getLexType() {return type;}
+	virtual int getType() { };
+	virtual int getValue(int a = 1, int b = 1) { };
 };
 
 class Number: public Lexem {
 	int value;
 public:
 	Number(int number);
-	int getValue() const;
+	int getValue(int, int);
 };
 
 Number::Number(int number) {
@@ -30,7 +31,8 @@ Number::Number(int number) {
 	type = NUMBER;
 }
 
-int Number::getValue() const {
+int Number::getValue(int a = 1, int b = 1) {
+//	cout << "num_get_value\n";
 	return value;
 }
 
@@ -50,7 +52,7 @@ class Oper: public Lexem {
 	OPERATOR opertype;
 public:
 	Oper(int charCode);
-	OPERATOR getType();
+	int getType();
 	int getPriority();
 	int getValue(int left, int right);
 };
@@ -60,21 +62,21 @@ Oper::Oper(int charCode = 0) {
 	switch(charCode) {
 		case '(': opertype = LBRACKET; break;
 		case ')': opertype = RBRACKET; break;
-		case '+': opertype = PLUS; break;
+		case '+': cout << PLUS << endl; opertype = PLUS; break;
 		case '-': opertype = MINUS; break;
 		case '*': opertype = MULTIPLY; break;
 	}
 }
 
-OPERATOR Oper::getType() {
+int Oper::getType() {
 	return opertype;
 }
 
 int Oper::getPriority() {
-	return PRIORITY[opertype];
+	return PRIORITY[opertype - 1];
 }
 
-int Oper::getValue(int left, int right) {
+int Oper::getValue(int left = 1, int right = 1) {
 	switch (this->getType()) {
 		case PLUS: return left + right;
 		case MINUS: return left - right;
@@ -83,29 +85,32 @@ int Oper::getValue(int left, int right) {
 }
 
 int getValue(int type, int left, int right) {
+//	cout << "getValue\n";
+//	cout << type << PLUS << endl;
 	switch (type) {
-		case PLUS: return left + right;
-		case MINUS: return left - right;
-		case MULTIPLY: return left * right;
+		case PLUS: cout << "PLUS\n"; return left + right;
+		case MINUS: cout << "MINUS\n"; return left - right;
+		case MULTIPLY: cout << "MULTIPLY\n"; return left * right;
 	}
 }
 
 vector<Lexem *> parseLexem(string codeline) {
 	vector<Lexem *> infix;
 	int i, j, number = 0;
-	for (i = 0; i < codeline.size(); i++) {
+	for (i = 0; i < codeline.size(); ++i) {
 		number = 0;
-		for (j = i; codeline[j] >= 0 && codeline[j] <= '9'; j++) {
+		for (j = i; codeline[j] >= '0' && codeline[j] <= '9'; ++j) {
 			number = number * 10 + codeline[j] - '0';
 		}
 		if (i != j) {
 			Number *num = new Number(number);
 			infix.push_back(num);
-			i = j;
-		}
-		else {
-			if (codeline[i] != ' ' && codeline[i] != '\t') {
+	//		cout << "num == " << number << endl;
+			i = j - 1;
+		} else {
+			if (codeline[i] != ' ' && codeline[i] != '\t' && codeline[i] != '\n') {
 				Oper *op = new Oper(codeline[i]);
+	//			cout << "op == " << codeline[i] << endl;
 				infix.push_back(op);
 			}
 		}
@@ -117,10 +122,13 @@ vector<Lexem *> buildPostfix(vector<Lexem *> infix) {
 	stack<Oper *> opstack;
 	vector<Lexem *> postfix;
 	for (int i = 0; i < infix.size(); i++) {
-		if (infix[i]->getType() == NUMBER)
+		if (infix[i]->getLexType() == NUMBER) {
 			postfix.push_back(infix[i]);
-		if (infix[i]->getType() == OPER) {
+	//		cout << "IS_NUMBER\n";
+		}
+		if (infix[i]->getLexType() == OPER) {
 			opstack.push((Oper *)infix[i]);
+	//		cout << "IS_OPER\n";
 		}
 	}
 	for (int i = opstack.size() - 1; i >= 0; i--) {
@@ -133,8 +141,11 @@ vector<Lexem *> buildPostfix(vector<Lexem *> infix) {
 int evaluatePostfix(vector<Lexem *> poliz) {
 	int value;
 	for (int i = 0; i < poliz.size(); i++) {
-		if (poliz[i]->getType() == OPER)
-			value = getValue(poliz[i]->getType(), poliz[i - 2]->getValue(), poliz[i - 1]->getValue());
+		if (poliz[i]->getLexType() == OPER) {
+		//	cout << poliz[i - 2]->getValue() << " " << poliz[i - 2]->getValue() << endl;
+		//	value = getValue(poliz[i]->getType(), poliz[i - 2]->getValue(), poliz[i - 1]->getValue());
+			value = poliz[i]->getValue(poliz[i - 2]->getValue(), poliz[i - 1]->getValue());
+		}
 	}
 	return value;
 }
