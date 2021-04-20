bec84f6bd01b877f48856541615ee19c9e19fb39
Valeriya Abdrakhmanova <valeriya.avt@gmail.com>
2021-04-20 19:23:26 +0200

    includes

---
+++ include/print.h
@@ -0,0 +1,4 @@
+void print(vector<Lexem *> &poliz, int index);
+void printMap(string lexemName, map<string, int> lexemesMap);
+void printArraysMap(map<string, Array *> lexemesMap);
+void printVector(string vectorName, vector<vector<Lexem *>> lexemes);
